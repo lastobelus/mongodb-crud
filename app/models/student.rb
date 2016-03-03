8a8a271cbd5e5002d341f541f6242298760246e0
Michael Johnston <lastobelus@mac.com>
2016-03-02 22:04:47 -0800

    add valiation of email & names

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,11 @@ class Student
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
+
+  validates :first_name, :last_name, presence: true
+
+  validates :email,
+    presence: true,
+    uniqueness: true
+
 end
